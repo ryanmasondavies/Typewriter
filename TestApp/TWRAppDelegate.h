76b6997eb230bdb27517eaa6bb7cca5a03eb5cfc
Ryan Davies <ryan@ryandavies.net>
2012-10-29 19:37:58 +0000

    Added test application and configured specs for application testing.
    This is so that Specs can use view controllers as part of a hierarchy, and so that touch events are handled properly.

---
+++ TestApp/TWRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TWRAppDelegate.h
+//  TestApp
+//
+//  Created by Ryan Davies on 29/10/2012.
+//  Copyright (c) 2012 Ryan Davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
